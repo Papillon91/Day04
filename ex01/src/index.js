b9ce4f727333e1aac1f67653a9371daf4520133c
almir <belive2018@hotmail.com>
2021-03-11 15:47:38 +0100

    try again

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,28 @@
+function main(numOne, numTwo){
 
+    var quotient;
+    if(numTwo === 0){
+        quotietn= 'Divide by 0! cannot divide by zero';
+    } else { 
+        // Only change code below this line
+         
+        var numOne;
+        var numTwo;
+        var quotient;
+
+        myres = numOne / numTwo;
+
+        // Only change code above this line
+
+    }
+
+    return quotient;
+
+}
+
+    console.log(main(15, 3));
+    console.log(main(25, 5));
+    console.log(main(420, 10));
+    console.log(main(55, 0));
+
+    module.exports = main;
---
+++ ex02/src/index.js
@@ -0,0 +1,23 @@
+function main(numOne, numTwo){
+
+    var myRes = 75;
+    
+     { 
+        // Only change code below this line
+
+        i++;
+
+        i = i + 1;
+
+        // Only change code above this line
+
+        return myRes;
+    
+    }
+     }
+
+
+
+
+    console.log(main(75));
+    module.exports = main;
